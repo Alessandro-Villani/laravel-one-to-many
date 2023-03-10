56817d2f6a24a234bca455c7efde7328000214bb
Alessandro-Villani <alessvill@libero.it>
2023-03-10 15:40:00 +0100

    display project type in index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -11,6 +11,7 @@
           <tr>
             <th scope="col">#</th>
             <th scope="col">Project Name</th>
+            <th scope="col">Type</th>
             <th scope="col" class="d-flex flex-column align-items-center justify-content-center">
                 Publication Status
                 <form id="filter-form" action="{{ route('admin.projects.index') }}" method="GET">
@@ -31,6 +32,7 @@
             <tr>
                 <th class="align-middle" scope="row">{{ $project->id }}</th>
                 <td class="align-middle">{{ $project->name }}</td>
+                <td class="align-middle">{{ $project->type ? $project->type->label : '-' }}</td>
                 <td class="align-middle">
                     <form class="mb-0 text-center" action="{{ route('admin.projects.toggle-status', $project->id) }}" method="POST">
                         @method('PATCH')
@@ -54,7 +56,7 @@
             </tr> 
             @empty
             <tr>
-                <th scope="row" colspan="6" class="text-center">Non ci sono progetti</th>
+                <th scope="row" colspan="7" class="text-center">Non ci sono progetti</th>
             </tr> 
             @endforelse
           
